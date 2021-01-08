6604b6b6f594b563f8c31e02cdc821eccec1c3ff
dinsogna <62686152+dinsogna@users.noreply.github.com>
2021-01-07 20:44:32 -0800

    Update test_pendulum.cc
    Added motor code; added dt calculations; added constants

--- gazebo_code/test_pendulum/test_pendulum.cc
+++ gazebo_code/test_pendulum/test_pendulum.cc
@@ -2,15 +2,86 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
+#include <gazebo/common/UpdateInfo.hh>
 #include <ignition/math/Vector3.hh>
+#include <eigen3/Eigen/Dense>
+#include <boost/bind.hpp>
+#include <stdio.h>
 #include <math.h>
+#include <vector>
+#include <gazebo/util/system.hh>
+
+
+
+
 
 namespace gazebo
 {
   class TestPendulum : public ModelPlugin
   {
+    const size_t N = 2;
+    typedef Eigen::VectorXd state_type;
+
+    //maybe set initial conditions here instead of in Load()??? call in load()
+    private: void ModelInit()
+    {
+
+    }
+
+    private:  state_type UpdateMotor(double t, state_type X) {
+      state_type state(N);
+
+      Eigen::MatrixXd A(2,2);
+      A << (-B/J), (K/J), (-K/L), (-R/L);
+      Eigen::MatrixXd B(2,2);
+      B << (-1/J), 0, 0, (1/L);
+      Eigen::MatrixXd C(2,1);
+      // C << Td, Vm; // do we need to add a Td parameter? Or store / update as global var
+
+      state = A*X + B*C;
+      return state;
+    }
+
+    private: state_type RK4_Step(double t, state_type state, double dt) 
+    {
+      double h = dt;
+      double h2 = 0.5*h;
+      double h6 = h/6.0;
+
+      state_type k1 = UpdateMotor(t, state);
+      state_type k2 = UpdateMotor(t + h2, state + h2*k1);
+      state_type k3 = UpdateMotor(t + h2, state + h2*k1);
+      state_type k4 = UpdateMotor(t + h, state + h*k3);
+    
+      return state + h6*(k1 + 2.0*(k2 + k3) + k4);
+  }
+
+
+
     public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
     {
+      
+      // TESTING ONLY
+      // state_type s1(2);
+      // s1(0) = 6;
+      // s1(1) = 9;
+      // std::cout<< "s1(0): " << s1(0) << std::endl;
+      // std::cout<< "s1(1): " << s1(1) << std::endl;
+      //
+
+      //========================
+      //INITIAL CONDITIONS
+      //========================
+      double theta=.3;      //initial theta (radians)
+      double theta_dot=0;  //initial theta dot (radians/s)
+      double iq=0;         //initial iq (amps)
+      double voltage=.07801;  //initial voltage to motor Vm (volts)
+      double torque=0;     //initial external torque (N*m)
+      double time=30;      //total time interval (seconds)
+      // double dt=0.0001;       //size of one time step (No longer need!)
+      ///////////////////////////
+
+
       // Store the pointer to the model
       this->model = _parent;
       // Sets the joint pointer
@@ -19,30 +90,44 @@ namespace gazebo
       this->L1=this->R1->GetChild();
       this->L0=this->R1->GetParent();
       //this->R1->SetProvideFeedback(true);
+      
+      //simTime starts at 0
+      this->lastUpdate = 0;
+
       // Listen to the update event. This event is broadcast every
       // simulation iteration.
       this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-          std::bind(&TestPendulum::OnUpdate, this));
+          std::bind(&TestPendulum::OnUpdate, this, std::placeholders::_1));
       //this->model->SetGravityMode(false);
     }
 
     // Called by the world update start event
-    public: void OnUpdate()
+    public: void OnUpdate(const common::UpdateInfo & _info)
     {
+      // Calculate the dt at each step
+      double dt = (_info.simTime - this->lastUpdate).Double();
+      this->lastUpdate = _info.simTime;
+      // std::cout << "dt: " << dt <<"; lastUpdate: " << this->lastUpdate<< "; simtime: " << _info.simTime << std::endl; //test 
+
       // Apply a small linear velocity to the model.
       //this->model->SetLinearVel(ignition::math::Vector3d(.3, .3, 0));
         count=count +.001;
         //this->R1->SetPosition(0,M_PI/4);
-        //std::cout<< this->R1->GetForceTorque(0).body1Torque << std::endl;
-        std::cout<< this->L1->RelativeAngularAccel()<< std::endl;
-        std::cout<< this->L1->RelativeAngularVel()<< std::endl;
-        std::cout<< this->L1->RelativeTorque()<< std::endl;
-        std::cout<< "" << std::endl;
+        //std::cout<< this->R1->GetForceTorque(0).body1Torque << std::endl; // WRONG
+        ignition::math::Vector3d t1(100,0,0);
+        // this->L1->AddTorque(t1);
+        // std::cout<< this->L1->RelativeAngularAccel()<< std::endl;
+        // std::cout<< this->L1->RelativeAngularVel()<< std::endl;
+        // std::cout<< this->L1->RelativeTorque()<< std::endl;
+        // std::cout<< "" << std::endl;
         //this->R1->GetVelocity(0);
+
+        
     }
 
     // Pointer to the model
     private:
+        //pointer to model
         physics::ModelPtr model;
         // Pointer to the joint
         physics::JointPtr R1;
@@ -52,7 +137,30 @@ namespace gazebo
         physics::LinkPtr L1;
         // Pointer to the update event connection
         event::ConnectionPtr updateConnection;
+
+        //SYSTEM CONSTANTS
         double count=0;
+        common::Time lastUpdate;
+        std::vector<state_type> values;
+        std::vector<double> T; // for motor only testing
+
+        //MOTOR CONSTANTS
+        double Vmax;
+        double R;
+        double L;
+        double K;
+        double B;
+        double J;
+        double Vm;
+        double Iq;
+        double gear_ratio; 
+        double relative_theta;
+
+        //PENDULUM CONSTANTS (set in .world file)
+        double g;
+        double l;
+        double b;
+        double m;
   };
 
   // Register this plugin with the simulator
